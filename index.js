d26ca1be680f7dd60395cf61c4ebd7f89896655f
Shaun Razak <shaun.f.razak@gmail.com>
2022-12-31 16:12:59 -0500

    commit tree 992f7a304b6b63ad457c68bd17900258212df6fb

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Follow along with the examples here
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+console.log(firstName)
+
+function say(greeting, firstName){
+    console.log(`${greeting}, ${firstName}!`);
+}
+say ("Julio", "Hello");
